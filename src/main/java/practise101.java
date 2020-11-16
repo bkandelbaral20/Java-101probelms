d06388fb2c8ea75db8bca2fbad75e2295ccb256e
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-11-15 19:39:46 -0600

    solving string problems

--- src/main/java/practise101.java
+++ src/main/java/practise101.java
@@ -1,5 +1,21 @@
+
+//java practise 101 challenge
+
 public class practise101 {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+
+//  * Exercise 001 - Add Ints
+//  * Write a method to add to integers (int) and return the sum.
+
+    public static int addInt(int a, int b){
+        return a+b;
     }
+
+//* Exercise 002 - Say Hello
+// * Given an input String, name; return a String with "Hello " appended in-front of name and "!" appended after name.
+
+    public static String sayHello( String name){
+        return "!" + name + "!";
+    }
+
+
 }
